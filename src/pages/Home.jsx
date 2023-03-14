742e7c43d8049251502e4a63d9b16b11c1b273d8
mehjwazir <mehjwazir@gmail.com>
2023-03-14 00:01:52 -0700

    updating home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,10 +17,37 @@ function Home() {
 			</div>
 			<hr />
 			<div>
-				<aside className='sidebar'>
+				<img src="grass2.png" alt="grass" className='grass' />
+			</div>
+			<div className='row'>
+				<div className='column'>
+		
+					<img src="Book.png" alt="book" className='book' />
+				</div>
+				<div className='column col2'>
 					<h4 >Subtle transformations</h4>
-				</aside>
-				<img src="Book.png" alt="book" className='book' />
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+			<section>
+				<div>
+					<h1>test</h1>
+				</div>
+			</section>
+			<div className='row row2'>
+			
+				<div className='column col2'>
+					<h4 >A guide to nurturing</h4>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+				<div className='column'>
+
+					<img src="center.png" alt="center" className='center' />
+				</div>
 			</div>
 		</main>
 	)
--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -1,10 +1,20 @@
 import './Quotes.css'
+import Img from './files/green.jpg'
 
 
 function Quotes() {
 	return (
 		<main>
-			<h1>Quotes</h1>
+			<div>
+				<h1>Quotes</h1>
+			</div>
+			<div>
+				<img src={Img}
+					alt="green quote"
+					className='green'
+				/>
+		</div>
+
 </main>
 	)
 }
