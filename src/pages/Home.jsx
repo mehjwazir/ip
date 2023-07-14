bdd2e3d6ea2485f4cb69eb7b196e78b87ccf10de
mehjwazir <mehjwazir@gmail.com>
2023-07-14 13:48:40 -0700

    added to home page new ui images and adjusted navbar css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,16 @@ function Home() {
 
 	return (
 		<main>
+			<div className='image-container'>
+				<div className="image">
+					<img src="ipmain.jpg" alt="" className='img1'/>
+				</div>
+				<div className="image">
+					<img src="ipmain2.JPG" alt="" className='img2'/>
+				</div>
+			</div>
+		
+
 	
 		</main>
 	)
