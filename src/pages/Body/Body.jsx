1d6ef1bca8cdb066db9968428a0ed2b2408fba93
mehjwazir <mehjwazir@gmail.com>
2023-07-14 01:54:35 -0700

    renamed functions and cleared content from old branch

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,37 +1,17 @@
 import './Body.css'
-import Img from './files/outer.png'
 
 
 
-function Outer() {
+
+function Body() {
 
 	return (
 		<main>
-			<div>
-				<img
-					src={Img}
-					alt="outer" />
-			</div>
-			<div>
-				<h1>The Outer is as important as the Inner</h1>
-				<h3>Body</h3>
-			</div>
-	 </main>
 
+		</main>
 
-
-
-
-
- )
-
-
-
-
-
-
-
+	)
 
 }
 
-export default Outer;
+export default Body;
--- src/pages/Mind/Mind.jsx
+++ src/pages/Mind/Mind.jsx
@@ -1,76 +1,15 @@
 import './Mind.css'
-import { useState } from 'react';
-import styled from "styled-components";
-import ReactPlayer from 'react-player';
-import Img from './files/inner.png'
-import Vid from './files/IP17.2.mp4'
+
 
 
 
 function Mind() {
-
-	const [isVideoLoaded, setIsVideoLoaded] = useState(false);
-	const onLoadedData = () => {
-		setIsVideoLoaded(true);
-	}
-
-	
 	return (
-		<div className='Inner'>
-			<Container>
-				<img
-					src={Img}
-					className="ip-video tiny"
-					alt="video"
-					style={{ opacity: isVideoLoaded ? 0 : 1 }}
-				/>
-
-				<div style={{ opacity: isVideoLoaded ? 1 : 0 }}>
-				
-					<ReactPlayer
-						url={Vid}
-						playing={true}
-						controls={true}
-						loop={true}
-						muted={true}
-						playsinline={true}
-						onReady={onLoadedData}
-						width={1820}
-						height={1000}
-					
-					/>
-
-				</div>
-			</Container>
-
-			<div>
-				<h1>Finding Inner Peace</h1>
+		<main>
 			
-			</div>
-		</div>
-		
-	);
+		</main>
+	)
 }
-
-const Container = styled.div`
-
- width: 100%;
-  position: relative;
-  display:flex;
-  justify-content: center;
-align-items: center;
-
-  .ip-video {
-
- position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    // transition: visibility 0ms ease 400ms;
-  }
-
-`;
 
 
 
