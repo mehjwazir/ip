a5edea3fd55bc52725bdb9986ef5dd353bfcc66e
mehjwazir <mehjwazir@gmail.com>
2023-07-16 00:24:47 -0700

    added img4 and img5 to homepage. Styledwith height width and margins

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,14 @@ function Home() {
 					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sin it occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
 					anim id est laborum.</p>
 				<img src="ipmain3.JPG" alt="" className='img3' />
-				
+			</div>
+			<div className='image-container img4-5-div'>
+				<div className="image">
+					<img src="ipmain4.JPG" alt="" className='img4' />
+				</div>
+				<div className="image">
+					<img src="ipmain5.JPG" alt="" className='img5' />
+				</div>
 			</div>
 		</main>
 	)
