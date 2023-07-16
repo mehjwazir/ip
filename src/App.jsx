966b75f4029b6657276b6a3586e1d1b696e42944
mehjwazir <mehjwazir@gmail.com>
2023-07-16 00:42:20 -0700

    added footer folder, footer.jsx and css files. Also included nature/journal routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 import Mind from './pages/Mind/Mind';
 import Body from './pages/Body/Body';
+import Nature from './pages/Nature/Nature';
+import Journal from './pages/Journal/Journal';
 
 
 function App() {
@@ -12,9 +15,12 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mindr" element={<Mind />} />
+        <Route path="/mind" element={<Mind />} />
         <Route path="/body" element={<Body />} />
+        <Route path="/nature" element={<Nature />} />
+        <Route path="/journal" element={<Journal />} />
       </Routes>
+      <Footer />
     </>
   );
 }
