e9df1a00f4bc25e88430973f64605282576b47ed
mehjwazir <mehjwazir@gmail.com>
2023-07-15 19:24:55 -0700

    deleted original setup for new branch merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import Home from './pages/Home';
-import Inner from './pages/Inner/Inner';
-import Inner from './pages/Outer/Outer';
+
+
+
 
 
 function App() {
   return (
     <>
-    <main className='App'>
-      <NavBar />
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Inner" element={<Inner />} />
-          <Route path="/Outer" element={<Outer />} />
-      </Routes>
+    <main>
+   
 
 
    </main>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,12 @@
 import './NavBar.css';
-import { Link } from 'react-router-dom';
+
 
 const NavBar = () => {
 	return (
 		<main>
-		<div className='ip-div'>
-				<img src="ip1.svg" alt="ip logo" className='ip-logo' />
-			</div>
-			<hr />
+	
 		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/inner">Inner</Link>
-				</li>
-				<li>
-					<Link to="/outer">Outer</Link>
-				</li>
-				<li>
-					<Link to="/nature">Nature</Link>
-				</li>
-				</ul>
-				
+		
 			</nav>
 		
 		</main>
