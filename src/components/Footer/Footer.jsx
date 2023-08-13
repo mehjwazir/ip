840e4a28d567500c2de3a0ab3e78d2e469d89a25
mehjwazir <mehjwazir@gmail.com>
2023-08-12 22:54:21 -0700

    added code for footer and footer.css not final testing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,20 @@
-import "./Footer.css"
-
-
-
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
 	return (
 		<footer>
-	
+			<div className="newsletter">
+				<input type="email" placeholder="Sign up for our newsletter" />
+				<button>Subscribe</button>
+			</div>
+			<div className="footer-links">
+				<h5>Contact</h5>
+				<h5>Retreats</h5>
+				<h5>Mindful Journal</h5>
+			</div>
 		</footer>
-	)
+	);
 }
 
-export default Footer();
+export default Footer;
