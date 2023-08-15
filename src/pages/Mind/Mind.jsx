a6b98c93f2446f964ec40136604ff2a45a2179bc
mehjwazir <mehjwazir@gmail.com>
2023-08-15 16:16:26 -0700

    added Img1 in Mind folder to files added code in Mind.jsx for img and paragraph added styling for both in Mind.css

--- src/pages/Mind/Mind.jsx
+++ src/pages/Mind/Mind.jsx
@@ -1,4 +1,5 @@
 import './Mind.css'
+import Img1 from "./files/img1.png"
 
 
 
@@ -6,7 +7,17 @@ import './Mind.css'
 function Mind() {
 	return (
 		<main className='main-mind'>
-			<h1>Mind</h1>
+		
+			<div className='div-img1-mind'>
+				<img src={Img1} alt="" className='img1-mind'/>
+			</div>
+			<div>
+				<p className='mind-h1'>Self-Care Is Gentle</p>
+				<br />
+				<p className='mind-p'>
+					Fresh Flowers. Waking up Early. Open Windows. Screnic Routes. Making Space for Better Things. Warm Nights. Heartfelt Conversations. 
+				</p>
+			</div>
 		</main>
 	)
 }
