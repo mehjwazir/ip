9fd63f303e4985fab7492c561c445063a2560350
mehjwazir <mehjwazir@gmail.com>
2023-08-15 22:54:36 -0700

    Added height and width to img1 in Mind.css, changed h5 in Journal.jsx, added margin and navbar styling in NavBar,jsx, tested margin in index.css

--- src/pages/Journal/Journal.jsx
+++ src/pages/Journal/Journal.jsx
@@ -6,7 +6,7 @@ function Journal() {
 		<main className='main-journal'>
 			<h1> Journal</h1>
 			<h3>Coming soon....</h3>
-			<h5>Interactive Personal Journal Notes</h5>
+			<h5>Interactive Personal Notes</h5>
 		</main>
 	)
 }
