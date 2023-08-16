1ddc5e55eb287253cb4729f18ab723ba5cd276ae
mehjwazir <mehjwazir@gmail.com>
2023-08-15 18:00:15 -0700

    added italic spans for paragraph in Mind.jsx styling in Mind.css added mobile ui

--- src/pages/Mind/Mind.jsx
+++ src/pages/Mind/Mind.jsx
@@ -12,10 +12,12 @@ function Mind() {
 				<img src={Img1} alt="" className='img1-mind'/>
 			</div>
 			<div>
-				<p className='mind-h1'>Self-Care Is Gentle</p>
+				<p className='mind-h1'>Self-Care Is <span className='italic'>Gentle</span></p>
 				<br />
 				<p className='mind-p'>
-					Fresh Flowers. Waking up Early. Open Windows. Screnic Routes. Making Space for Better Things. Warm Nights. Heartfelt Conversations. 
+					Fresh Flowers. Waking <span className='italic'>up</span> Early. Open Windows. <br />
+					<span className='italic'>Scenic</span> Routes. Making Space for Better Things. <br />
+					Warm Nights. <span className='italic'>Heartfelt </span> Conversations. 
 				</p>
 			</div>
 		</main>
