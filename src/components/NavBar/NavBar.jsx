ee42d842cf4fe7dee442e6cdd6d75c0901ccb747
mehjwazir <mehjwazir@gmail.com>
2023-08-16 23:08:56 -0700

    added images to NavBar folder files Created a drop down navbar menu with images/links styled ui for navbar.css fixed position

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,49 @@
 import './NavBar.css';
 import { Link } from "react-router-dom";
+import Img1 from './files/img1.JPG'
+import Img2 from './files/img2.JPG'
+import Img3 from './files/img3.jpg'
+import Img4 from './files/img4.jpg'
 
 
 const NavBar = () => {
 	return (
 		<main>
-	
-				<nav className='navbar'>
-					<ul>
-						<li>
-							<Link to="/">Inner Peace</Link>
-						</li>
-						<li>
-							<Link to="/mind">Mind</Link>
-						</li>
-						<li>
-							<Link to="/body">Body</Link>
-						</li>
-						<li>
-							<Link to="/nature">Nature</Link>
-						</li>
-						<li>
-							<Link to="/journal">Journal</Link>
-						</li>
-					</ul>
+
+			<nav className='navbar'>
+				<ul>
+					<li>
+						<Link to="/">Inner Peace</Link>
+					</li>
+					<li className="dropdown">
+						<Link className='discover-pointer'>Discover</Link>	
+						<div className="dropdown-content">
+							<Link to="/mind">
+								<img src={Img1} alt="Mind" />
+								Mind
+							</Link>
+							<Link to="/body">
+								<img src={Img2} alt="Body" />
+								Body
+							</Link>
+							<Link to="/nature">
+								<img src={Img3} alt="Nature" />
+								Nature
+							</Link>
+							<Link to="/journal">
+								<img src={Img4} alt="Journal" />
+								Journal
+							</Link>
+
+						</div>
+					</li>
+
+				</ul>
 			</nav>
-		
+
+
 		</main>
 	);
 };
 
-export default NavBar;
+export default NavBar; 
