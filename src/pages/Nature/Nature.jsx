b52605d6a90c92b581d88be02cfaee92b42805f3
mehjwazir <mehjwazir@gmail.com>
2023-08-22 22:25:30 -0700

    added a few images to folders testing ui in Nature.jsx/css

--- src/pages/Nature/Nature.jsx
+++ src/pages/Nature/Nature.jsx
@@ -1,10 +1,12 @@
 import './Nature.css';
-
+import Img1 from './files/1.jpg'
 
 function Nature() {
 	return (
 		<main className='main-nature'>
-			<h1>Nature</h1>
+			<div>
+				<img src={Img1} alt="" className='nature-img1' />
+			</div>
 		</main>
 	)
 }
