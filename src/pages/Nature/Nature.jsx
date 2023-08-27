6eeda4ee8d522c6eae5ba7a8a14465aef074e45b
mehjwazir <mehjwazir@gmail.com>
2023-08-26 22:05:24 -0700

    added img2 to nature.jsx and styling to nature.css

--- src/pages/Nature/Nature.jsx
+++ src/pages/Nature/Nature.jsx
@@ -1,11 +1,15 @@
 import './Nature.css';
 import Img1 from './files/1.jpg'
+import Img2 from './files/2.JPG'
 
 function Nature() {
 	return (
 		<main className='main-nature'>
 			<div>
 				<img src={Img1} alt="" className='nature-img1' />
+			</div>
+			<div>
+			<img src={Img2} alt="" className='nature-img2' />
 			</div>
 		</main>
 	)
