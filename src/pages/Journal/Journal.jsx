a9f8743d1303e101211d7b9df187fb44bc3c6679
mehjwazir <mehjwazir@gmail.com>
2023-09-22 23:56:22 -0700

    added journal img in files folder styled in Journal.css and adjusted code in Journal.jsx

--- src/pages/Journal/Journal.jsx
+++ src/pages/Journal/Journal.jsx
@@ -1,14 +1,11 @@
 import './Journal.css';
-import Img1 from './files/1.jpg'
+import Img1 from './files/1.png'
 
 
 function Journal() {
 	return (
 		<>
 		<main className='main-journal'>
-			<h1> Journal</h1>
-			<h3>Coming soon....</h3>
-			<h5>Interactive Personal Notes</h5>
 			<img src={Img1} alt="" className='img1-journal' />
 			</main>
 		</>
