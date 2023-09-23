c510fadfc3226cbd10ac82a8483f43f599365b4d
mehjwazir <mehjwazir@gmail.com>
2023-09-22 22:40:16 -0700

    added Img3.png to Nature folder files, added section with div inside and changed import of Img1 in Nature.jsx

--- src/pages/Nature/Nature.jsx
+++ src/pages/Nature/Nature.jsx
@@ -1,14 +1,17 @@
 import './Nature.css';
-import Img1 from './files/1.jpg'
+import Img1 from './files/Img3.png'
 import Img2 from './files/2.JPG'
 
 function Nature() {
 	return (
 		<>
-		<main className='main-nature'>
-			<div>
-				<img src={Img1} alt="" className='nature-img1' />
-			</div>
+			<main className='main-nature'>
+				<section>
+					<div className='nature-div-1'>
+						<img src={Img1} alt="" className='nature-img1' />
+					</div>
+				</section>
+			
 			<div>
 			<img src={Img2} alt="" className='nature-img2' />
 			</div>
