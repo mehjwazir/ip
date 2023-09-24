d0f47b0be1a97d7efc967788f55df21d68f8b7a0
mehjwazir <mehjwazir@gmail.com>
2023-09-24 00:33:48 -0700

    Changed file for Img1 to 4.jpeg added paragraph next to home-img1 styled text to inline next to image.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import './Home.css';
+import Img1 from './files/4.jpeg'
 
 function Home() {
 
@@ -8,17 +9,18 @@ function Home() {
 				<header className='home-header'>
 					<h1 className='home-h1'>Inner Peace <span className='home-long-dash'>&#x2015;&#x2015;</span> <span className='home-span'>for</span> the <br /><span className='home-span'>Mind</span> Body & <span className='home-span'>Soul</span></h1>
 				</header>
-				<hr />
+
 				<section className='home-section1'>
 					<div className='home-div1'>
-
+						<img src={Img1} alt="" className='home-img1' />
+						<div className='home-text'>
+							<p className='home-paragraph'>A place to calm the senses.</p>
+						</div>
 					</div>
 				</section>
-				
-		</main>
+			</main>
 		</>
 	)
-
 }
 
 
