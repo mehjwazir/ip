cfdcf1aedcb87549b29aee3d922ffb808ff42727
mehjwazir <mehjwazir@gmail.com>
2023-09-23 20:55:24 -0700

    added main, header, section and divs with classes in Home.jsx styled the header in Home.css. Fixed NavBar dropdown content with z-index:1 in NavBar.css because of <hr/> showing over it from header.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,18 @@ function Home() {
 
 	return (
 		<>
-		
+			<main className='home-main'>
+				<header className='home-header'>
+					<h1 className='home-h1'>Inner Peace <span className='home-long-dash'>&#x2015;&#x2015;</span> <span className='home-span'>for</span> the <br /><span className='home-span'>Mind</span> Body & <span className='home-span'>Soul</span></h1>
+				</header>
+				<hr />
+				<section className='home-section1'>
+					<div className='home-div1'>
+
+					</div>
+				</section>
+				
+		</main>
 		</>
 	)
 
