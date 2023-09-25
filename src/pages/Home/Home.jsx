e48287b0c90a6d80865d67606e34378d56281ef8
mehjwazir <mehjwazir@gmail.com>
2023-09-25 15:39:41 -0700

    added section and div in Home.jsx styled in Home.css added imgs in files folder. Changed background color in index.css.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,48 @@
 import './Home.css';
 import Img1 from './files/4.jpeg'
+import Img2 from './files/6.jpeg'
+
 
 function Home() {
+
 
 	return (
 		<>
 			<main className='home-main'>
 				<header className='home-header'>
-					<h1 className='home-h1'>Inner Peace <span className='home-long-dash'>&#x2015;&#x2015;</span> <span className='home-span'>for</span> the <br /><span className='home-span'>Mind</span> Body & <span className='home-span'>Soul</span></h1>
+					<h1 className='home-h1 small-screen' >
+						Inner Peace <span className='home-long-dash'>&#x2015;&#x2015;</span> <span className='home-span'>for</span> the <br /><span className='home-span'>Mind</span> Body & <span className='home-span'>Soul</span>
+					</h1>
 				</header>
-
 				<section className='home-section1'>
-					<div className='home-div1'>
-						<img src={Img1} alt="" className='home-img1' />
-						<div className='home-text'>
-							<p className='home-paragraph'>A place to calm the senses.</p>
+					<div className='row'>
+						<div className='col-md-6'>
+							<img src={Img1} alt='' className='home-img1' />
+						</div>
+						<div className='col-md-6'>
+							<div className='home-text'>
+								<p>A place to calm the senses.</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				<div className='home-marq'>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut amet minima quasi adipisci nihil? Numquam voluptatum assumenda suscipit, est recusandae nobis quas quisquam quo, dolor fugit non animi delectus velit. </p>
+				</div>
+				<section className='home-section2'>
+					<div className='row'>
+						<div className='col-md-6'>
+							<div className='home-text2'>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex esse doloribus vero neque qui, laudantium vitae. Maxime inventore rerum eius? Illo, architecto! Sapiente, consectetur consequatur esse corrupti sed in illum.</p>
+							</div>
+						</div>
+						<div className='col-md-6'>
+							<img src={Img2} alt='' className='home-img2' />
 						</div>
 					</div>
 				</section>
 			</main>
+			
 		</>
 	)
 }
