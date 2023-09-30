75112b8e00a3864cfa4d1250d6322ad8dfc19989
mehjwazir <mehjwazir@gmail.com>
2023-09-29 22:54:21 -0700

    commented out App.css code. Added a few images in Mind files folder. testing ui in Mind.jsx styled in Mind.css

--- src/pages/Mind/Mind.jsx
+++ src/pages/Mind/Mind.jsx
@@ -1,5 +1,9 @@
 import './Mind.css'
 import Img1 from './files/2.png'
+import Img2 from './files/3.jpeg'
+import Img3 from './files/4.jpeg'
+import Img4 from './files/5.png'
+import Img5 from './files/6.png'
 
 
 
@@ -8,12 +12,29 @@ import Img1 from './files/2.png'
 function Mind() {
 	return (
 		<>
-			<main>
+			<main className='mind-main'>
 				<section className='mind-section'>
+				
 					<div>
-						<img src={Img1} alt="" className='mind-img1'/>
+						<img src={Img2} alt="" className='mind-img2'/>
 					</div>
+					<div>
+						<img src={Img1} alt="" className='mind-img1' />
+					</div>
+				
+					<div>
+						<img src={Img4} alt="" className='mind-img4'/>
+					</div>
+					
 				</section>
+				<div className='mind-div'>
+					<div>
+						<img src={Img3} alt="" className='mind-img3' />
+					</div>
+					<div>
+						<img src={Img5} alt="" className='mind-img5' />
+					</div>
+				</div>
 
 			</main>
 		</>
