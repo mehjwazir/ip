48a82bb10e9796fa8b6fb394397ed29f6d088b9a
mehjwazir <mehjwazir@gmail.com>
2023-10-09 08:49:58 -0700

    added images to Body folder files added code to Body.jsx added imports

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import './Body.css';
+import Img1 from './files/1.png'
 
 
 
@@ -7,11 +9,28 @@ function Body() {
 
 	return (
 		<>
-	
+			<main className='body-main'>
+			<div className='image-card-container'>
+				<div className='image-card'>
+					<Link to={'/post1'}>
+						<img src={Img1} alt='' />
+					</Link>
+					<div className='text-container'>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+							repudiandae nisi aperiam magnam veritatis, temporibus vel
+							maxime saepe expedita iste doloremque sed eveniet veniam rem
+							explicabo, accusamus quos. Quidem, rem.
+						</p>
+					</div>
+					</div>
+				</div>
+
+			</main>
 		</>
 
 	)
-
 }
+
 
 export default Body;
