2b96837dd98f1b67f5249bdc691ff3737de97c51
mehjwazir <mehjwazir@gmail.com>
2023-10-09 07:56:49 -0700

    adjusted the code in Home.jsx and Home.css to be responsive still designing ui. Deleted pictures in Home files folder and Mind files. Updated code in Mind.jsx and Mind.css added custom design pictures.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,29 @@
 import './Home.css';
-import Img1 from './files/10.jpeg'
-import Img2 from './files/9.jpeg'
-import Img3 from './files/7.jpeg'
-
+import Img1 from './files/1.jpg';
+import Img2 from './files/9.jpeg';
+import Img3 from './files/12.jpg';
 
 function Home() {
-
-
 	return (
 		<>
 			<main className='home-main'>
 				<header className='home-header'>
-					<h1 className='home-h1 small-screen' >
-						Inner Peace <span className='home-long-dash'>&#x2015;&#x2015;</span> <span className='home-span'>for</span> the <br /><span className='home-span'>Mind</span> Body & <span className='home-span'>Soul</span>
+					<h1 className='home-h1' >
+						Inner Peace <span className='home-span'>&#x2015;</span> <span className='home-span'>for</span> the <br /><span className='home-span'>Mind</span> Body & <span className='home-span'>Soul</span>
 					</h1>
 				</header>
 				<section className='home-section1'>
 					<div className='row'>
 						<div className='col-md-6'>
-							<img src={Img1} alt='' className='home-img1' />
+							<img src={Img1} alt='' className='home-img' />
 						</div>
-						<div className='col-md-6'>
-							<div className='home-text'>
-								<p>
-									<span className='home-span2'>A </span>
-									<span className='home-span2'>Place</span> to 
-									<span className='home-span2'> calm </span>the
-									<span className='home-span2'> senses</span>.
-								</p>
-							</div>
+						<div className='col-md-6 home-text'>
+							<p>
+								<span className='home-span2'>A </span>
+								<span className='home-span2'>Place</span> to
+								<span className='home-span2'> calm </span>the
+								<span className='home-span2'> senses</span>.
+							</p>
 						</div>
 					</div>
 				</section>
@@ -37,36 +32,27 @@ function Home() {
 				</div>
 				<section className='home-section2'>
 					<div className='row'>
-						<div className='col-md-6'>
-							<div className='home-text2'>
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex esse doloribus vero neque qui, laudantium vitae. Maxime inventore rerum eius? Illo, architecto! Sapiente, consectetur consequatur esse corrupti sed in illum.</p>
-							</div>
+						<div className='col-md-6 home-text2'>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex esse doloribus vero neque qui, laudantium vitae. Maxime inventore rerum eius? Illo, architecto! Sapiente, consectetur consequatur esse corrupti sed in illum.</p>
 						</div>
 						<div className='col-md-6'>
-							<img src={Img2} alt='' className='home-img2' />
+							<img src={Img2} alt='' className='home-img' />
 						</div>
 					</div>
 				</section>
 				<section className='home-section2'>
 					<div className='row'>
 						<div className='col-md-6'>
-							<img src={Img3} alt='' className='home-img2' />
+							<img src={Img3} alt='' className='home-img' />
 						</div>
-						<div className='col-md-6'>
-							<div className='home-text2'>
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex esse doloribus vero neque qui, laudantium vitae. Maxime inventore rerum eius? Illo, architecto! Sapiente, consectetur consequatur esse corrupti sed in illum.</p>
-							</div>
+						<div className='col-md-6 home-text2'>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex esse doloribus vero neque qui, laudantium vitae. Maxime inventore rerum eius? Illo, architecto! Sapiente, consectetur consequatur esse corrupti sed in illum.</p>
 						</div>
-						
 					</div>
 				</section>
-				{/* <div className='lower-banner'>
-					<h1 className='banner-h1'>Let's Peace & Quiet Here</h1>
-				</div> */}
 			</main>
-
 		</>
-	)
+	);
 }
 
 export default Home;
--- src/pages/Mind/Mind.jsx
+++ src/pages/Mind/Mind.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import './Mind.css';
 import { Link } from 'react-router-dom';
 import Img1 from './files/1.png';
-import Img2 from './files/8.jpeg';
+import Img2 from './files/2.png';
+import Img3 from './files/3.jpeg';
+import Img4 from './files/4.png';
+import Img5 from './files/5.jpeg';
 
 function Mind() {
 	return (
 		<>
 			<main className='mind-main'>
 				<div className='large-image'>
-					<img src={Img2} alt='' className='col-img' />
-					<img src={Img1} alt='' className='col-img'/>
-					<img src={Img1} alt='' className='col-img'/>
-					<img src={Img1} alt='' className='col-img'/>
-					<img src={Img1} alt='' className='col-img'/>
-					<img src={Img1} alt='' className='col-img'/>
+					<img src={Img3} alt='' className='col-img' />
+					<img src={Img4} alt='' className='col-img' />
+					<img src={Img5} alt='' className='col-img' />
+					<img src="" alt='' className='col-img' />
+					<img src="" alt='' className='col-img' />
+					<img src="" alt='' className='col-img' />
 				</div>
 				<div className='image-card-container'>
 					<div className='image-card'>
@@ -32,7 +35,7 @@ function Mind() {
 					</div>
 					<div className='image-card'>
 						<Link to={'/post1'}>
-							<img src={Img1} alt='' />
+							<img src={Img2} alt='' />
 						</Link>
 						<div className='text-container'>
 							<p>
@@ -43,9 +46,6 @@ function Mind() {
 							</p>
 						</div>
 					</div>
-			
-					
-					
 				</div>
 			</main>
 		</>
