9cdcce4168421d2d0ce711d0c8370946b185554d
mehjwazir <mehjwazir@gmail.com>
2023-10-09 13:03:28 -0700

    added images in body folder files added new code in Body.jsx and Body.css styled page adding and testing things.

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
 import './Body.css';
 import Img1 from './files/1.png'
+import Img2 from './files/1.jpeg'
+import Img3 from './files/4.jpeg'
+import Img4 from './files/3.png'
 
 
 
@@ -9,8 +12,8 @@ function Body() {
 
 	return (
 		<>
-			<main className='body-main'>
-			<div className='image-card-container'>
+			
+			<div className='image-card-container margin-main'>
 				<div className='image-card'>
 					<Link to={'/post1'}>
 						<img src={Img1} alt='' />
@@ -23,9 +26,34 @@ function Body() {
 							explicabo, accusamus quos. Quidem, rem.
 						</p>
 					</div>
+				</div>
+			</div>
+			<main className='body-main'>
+			
+			
+				<div className='large-image'>
+					<img src={Img2} alt='' className='col-img' />
+					<img src={Img3} alt='' className='col-img' />
+					<img src={Img4} alt='' className='col-img' />
+					<img src="" alt='' className='col-img' />
+					<img src="" alt='' className='col-img' />
+					<img src="" alt='' className='col-img' />
+				</div>
+				<div className='image-card-container'>
+					<div className='image-card'>
+						<Link to={'/post1'}>
+							<img src={Img1} alt='' />
+						</Link>
+						<div className='text-container'>
+							<p>
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+								repudiandae nisi aperiam magnam veritatis, temporibus vel
+								maxime saepe expedita iste doloremque sed eveniet veniam rem
+								explicabo, accusamus quos. Quidem, rem.
+							</p>
+						</div>
 					</div>
 				</div>
-
 			</main>
 		</>
 
