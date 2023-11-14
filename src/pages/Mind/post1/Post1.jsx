6981018a8caa51ff4cce28ae71146c06e5074fbf
mehjwazir <mehjwazir@gmail.com>
2023-11-14 00:38:20 -0800

    added code in Post1.jsx and Post1.css

--- src/pages/Mind/post1/Post1.jsx
+++ src/pages/Mind/post1/Post1.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './Post1.css';
+
 
 export default function Post1() {
 	return (
@@ -6,7 +8,14 @@ export default function Post1() {
 	  
 			<main>
 				<div>
-					<h1>Test</h1>
+					<div className='div1-post'>
+						<h1>Post 1</h1>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates eaque, reiciendis, nemo sint quis cum, illum voluptate sapiente eius incidunt itaque magni consectetur. Minus a illo asperiores veritatis cumque beatae?
+
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, neque! Fugit, cumque alias excepturi animi sit temporibus magni facere. Distinctio dolorem animi voluptas odit culpa quo ad. Et, illum animi?
+
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis, quas magni, modi aut, nesciunt mollitia neque nisi dicta ipsa omnis vero. Sunt fuga cupiditate dolorem, eligendi necessitatibus labore pariatur unde?
+					</div>
 				</div>
 	  </main>
 	  
