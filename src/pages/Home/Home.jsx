fae2c752a576f2be65e09f583c35d34e595eb187
mehjwazir <mehjwazir@gmail.com>
2023-11-19 23:57:11 -0800

    added buttons on homepage for mindset shifts section links lead to different pages tranq, zen , solace, fluidity. Also added pictures of buttons in Home files folder

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
 import Img1 from './files/1.jpg';
 import Img2 from './files/9.jpeg';
 import Img3 from './files/12.jpg';
 // import Img4 from './files/15.png';
 // import Img5 from './files/16.jpeg';
 import Img6 from './files/17.png';
-import Img7 from './files/20.png';
+import Img7 from './files/22.png';
+import Img8 from './files/21.png';
+import Img9 from './files/2.png';
+import Img10 from './files/3.png';
+import Img11 from './files/4.png';
 
 function Home() {
 	return (
@@ -39,8 +44,22 @@ function Home() {
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut amet minima quasi adipisci nihil? Numquam voluptatum assumenda suscipit, est recusandae nobis quas quisquam quo, dolor fugit non animi delectus velit. </p>
 				</div>
 				<div className='div-5'>
-					{/* <img src={Img5} alt="" className='img5-home' /> */}
 					<img src={Img7} alt="" className='img7-home' />
+					<div className='button-container'>
+						{/* Use Link instead of button for navigation */}
+						<Link to='/tranquility' >
+							<img src={Img8} alt="" className='btn-t' />
+						</Link>
+						<Link to='/zen' >
+							<img src={Img9} alt="" className='btn-t' />
+						</Link>
+						<Link to='/solace' >
+							<img src={Img10} alt="" className='btn-t' />
+						</Link>
+						<Link to='/fluidity' >
+							<img src={Img11} alt="" className='btn-t' />
+						</Link>
+					</div>
 				</div>
 				<div className='home-marq'>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut amet minima quasi adipisci nihil? Numquam voluptatum assumenda suscipit, est recusandae nobis quas quisquam quo, dolor fugit non animi delectus velit. </p>
