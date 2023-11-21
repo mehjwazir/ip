12afd7f75d3cadc5e6b5929ca0c1658711f9bb3a
mehjwazir <mehjwazir@gmail.com>
2023-11-20 23:19:48 -0800

    removed imports in home.jsx that were not being used deleted code that wasn't used. Added new imgs and imports organized code and added slider in div-3 and organzied css code and classes in home.css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,23 @@
 import './Home.css';
 import { Link } from 'react-router-dom';
-import Img1 from './files/1.jpg';
-import Img2 from './files/9.jpeg';
-import Img3 from './files/12.jpg';
-// import Img4 from './files/15.png';
-// import Img5 from './files/16.jpeg';
-import Img6 from './files/17.png';
-import Img7 from './files/22.png';
-import Img8 from './files/21.png';
-import Img9 from './files/2.png';
-import Img10 from './files/3.png';
-import Img11 from './files/4.png';
+import Img1 from './files/1.png';
+import Img2 from './files/2.jpg';
+import Img3 from './files/3.png';
+import Img4 from './files/4.png';
+import Img5 from './files/5.png';
+import Img6 from './files/6.png';
+import Img7 from './files/7.png';
+import Img8 from './files/8.png';
+import Img9 from './files/9.jpeg';
+import Img10 from './files/10.jpg';
 
 function Home() {
 	return (
 		<>
 			<main className='home-main'>
-				<div className='img-6'>
-					<img src={Img6} alt="" className='img6' />
+				<div className='div-1'>
+					<img src={Img1} alt="" className='img1' />
 				</div>
-		
 				<header className='home-header'>
 					<h1 className='home-h1' >
 						Inner Peace <span className='home-span'>&#x2015;</span> <span className='home-span'>for</span> the <br /><span className='home-span'>Mind</span> Body & <span className='home-span'>Soul</span>
@@ -28,7 +26,7 @@ function Home() {
 				<section className='home-section1'>
 					<div className='row'>
 						<div className='col-md-6'>
-							<img src={Img1} alt='' className='home-img' />
+							<img src={Img2} alt='' className='home-img' />
 						</div>
 						<div className='col-md-6 home-text'>
 							<p>
@@ -43,24 +41,46 @@ function Home() {
 				<div className='home-marq'>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut amet minima quasi adipisci nihil? Numquam voluptatum assumenda suscipit, est recusandae nobis quas quisquam quo, dolor fugit non animi delectus velit. </p>
 				</div>
-				<div className='div-5'>
-					<img src={Img7} alt="" className='img7-home' />
-					<div className='button-container'>
-						{/* Use Link instead of button for navigation */}
-						<Link to='/tranquility' >
-							<img src={Img8} alt="" className='btn-t' />
-						</Link>
-						<Link to='/zen' >
-							<img src={Img9} alt="" className='btn-t' />
-						</Link>
-						<Link to='/solace' >
-							<img src={Img10} alt="" className='btn-t' />
-						</Link>
-						<Link to='/fluidity' >
-							<img src={Img11} alt="" className='btn-t' />
-						</Link>
+		
+
+				<div className='img-container' >
+					<div>
+							<img src={Img3} alt="" className='img3-home' />
+							<div className='button-container'>
+								<Link to='/tranquility' >
+									<img src={Img4} alt="" className='btn-t' />
+								</Link>
+								<Link to='/zen' >
+									<img src={Img5} alt="" className='btn-t' />
+								</Link>
+								<Link to='/solace' >
+									<img src={Img6} alt="" className='btn-t' />
+								</Link>
+								<Link to='/fluidity' >
+									<img src={Img7} alt="" className='btn-t' />
+								</Link>
+
+							</div>
 					</div>
-				</div>
+					<div className='scroll-container'>
+						<img src={Img8} alt="" className='img8-home' />
+						<img src={Img8} alt="" className='img8-home' />
+						<img src={Img8} alt="" className='img8-home' />
+						<img src={Img8} alt="" className='img8-home' />
+						<img src={Img8} alt="" className='img8-home' />
+						<img src={Img8} alt="" className='img8-home' />
+						<img src={Img8} alt="" className='img8-home' />
+					</div>
+						</div>
+					
+					
+				
+			
+
+
+			
+
+
 				<div className='home-marq'>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut amet minima quasi adipisci nihil? Numquam voluptatum assumenda suscipit, est recusandae nobis quas quisquam quo, dolor fugit non animi delectus velit. </p>
 				</div>
@@ -70,15 +90,15 @@ function Home() {
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex esse doloribus vero neque qui, laudantium vitae. Maxime inventore rerum eius? Illo, architecto! Sapiente, consectetur consequatur esse corrupti sed in illum.</p>
 						</div>
 						<div className='col-md-6'>
-							<img src={Img2} alt='' className='home-img' />
+							<img src={Img9} alt='' className='home-img' />
 						</div>
 					</div>
 				</section>
-			
+
 				<section className='home-section2'>
 					<div className='row'>
 						<div className='col-md-6'>
-							<img src={Img3} alt='' className='home-img' />
+							<img src={Img10} alt='' className='home-img' />
 						</div>
 						<div className='col-md-6 home-text2'>
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex esse doloribus vero neque qui, laudantium vitae. Maxime inventore rerum eius? Illo, architecto! Sapiente, consectetur consequatur esse corrupti sed in illum.</p>
