6c0a00119c1668cd4304a8ff87efad1f1f55b127
mehjwazir <mehjwazir@gmail.com>
2023-12-03 15:01:05 -0800

    replaced imgs for home.jsx added buttons in slider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,8 +8,12 @@ import Img5 from './files/5.png';
 import Img6 from './files/6.png';
 import Img7 from './files/7.png';
 import Img8 from './files/8.png';
-import Img9 from './files/9.jpeg';
-import Img10 from './files/10.jpg';
+import Img9 from './files/9.png';
+import Img10 from './files/10.png';
+import Img11 from './files/11.png';
+import Img12 from './files/12.png';
+import Img99 from './files/99.jpeg';
+import Img100 from './files/100.jpg';
 
 function Home() {
 	return (
@@ -41,46 +45,37 @@ function Home() {
 				<div className='home-marq'>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut amet minima quasi adipisci nihil? Numquam voluptatum assumenda suscipit, est recusandae nobis quas quisquam quo, dolor fugit non animi delectus velit. </p>
 				</div>
-		
+
 
 				<div className='img-container' >
 					<div>
-							<img src={Img3} alt="" className='img3-home' />
-							<div className='button-container'>
-								<Link to='/tranquility' >
-									<img src={Img4} alt="" className='btn-t' />
-								</Link>
-								<Link to='/zen' >
-									<img src={Img5} alt="" className='btn-t' />
-								</Link>
-								<Link to='/solace' >
-									<img src={Img6} alt="" className='btn-t' />
-								</Link>
-								<Link to='/fluidity' >
-									<img src={Img7} alt="" className='btn-t' />
-								</Link>
+						<img src={Img3} alt="" className='img3-home' />
+						<div className='button-container'>
+							<Link to='/tranquility' >
+								<img src={Img4} alt="" className='btn-t' />
+							</Link>
+							<Link to='/zen' >
+								<img src={Img5} alt="" className='btn-t' />
+							</Link>
+							<Link to='/solace' >
+								<img src={Img6} alt="" className='btn-t' />
+							</Link>
+							<Link to='/fluidity' >
+								<img src={Img7} alt="" className='btn-t' />
+							</Link>
 
-							</div>
+						</div>
 					</div>
 					<div className='scroll-container'>
 						<img src={Img8} alt="" className='img8-home' />
-						<img src={Img8} alt="" className='img8-home' />
-						<img src={Img8} alt="" className='img8-home' />
-						<img src={Img8} alt="" className='img8-home' />
-						<img src={Img8} alt="" className='img8-home' />
+						<img src={Img9} alt="" className='img8-home' />
+						<img src={Img10} alt="" className='img8-home' />
+						<img src={Img11} alt="" className='img8-home' />
+						<img src={Img12} alt="" className='img8-home' />
 						<img src={Img8} alt="" className='img8-home' />
 						<img src={Img8} alt="" className='img8-home' />
 					</div>
-						</div>
-					
-					
-				
-			
-
-
-			
-
-
+				</div>
 				<div className='home-marq'>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut amet minima quasi adipisci nihil? Numquam voluptatum assumenda suscipit, est recusandae nobis quas quisquam quo, dolor fugit non animi delectus velit. </p>
 				</div>
@@ -90,7 +85,7 @@ function Home() {
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex esse doloribus vero neque qui, laudantium vitae. Maxime inventore rerum eius? Illo, architecto! Sapiente, consectetur consequatur esse corrupti sed in illum.</p>
 						</div>
 						<div className='col-md-6'>
-							<img src={Img9} alt='' className='home-img' />
+							<img src={Img99} alt='' className='home-img' />
 						</div>
 					</div>
 				</section>
@@ -98,7 +93,7 @@ function Home() {
 				<section className='home-section2'>
 					<div className='row'>
 						<div className='col-md-6'>
-							<img src={Img10} alt='' className='home-img' />
+							<img src={Img100} alt='' className='home-img' />
 						</div>
 						<div className='col-md-6 home-text2'>
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex esse doloribus vero neque qui, laudantium vitae. Maxime inventore rerum eius? Illo, architecto! Sapiente, consectetur consequatur esse corrupti sed in illum.</p>
